836fb6b9b956fa00149101c189e8eb3fa24dbc74
abbbhucho <abcbhucho@gmail.com>
2024-04-11 01:32:20 +0530

    257. Binary Tree Paths

---
+++ 257. Binary Tree Paths/Solution.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution{
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        TreeNode *t = root;
+        vector<string> output;
+        string op = "";
+        stack<TreeNode *> st;
+        while((t->left != NULL || t->right != NULL) || !st.empty()){
+            // if(str.find())
+            t == root ? op.append(to_string(t->val)) : op.append("->" + to_string(t->val));
+            if(t->left != NULL){
+                st.emplace(t);
+                t = t->left;
+            } else if(t->right != NULL) {
+                st.emplace(t);
+                t = t->right;
+            } else {
+                output.push_back(op);
+                TreeNode *temp = t;
+                t = st.top();
+                if( t->left == temp) {
+                    t->left = NULL;
+                    temp = NULL;
+                    op.erase(op.end()-3);
+                } else if(t->right == temp) {
+                    t->right == NULL;
+                    temp = NULL;
+                    op.erase(op.end()-3);
+                }
+                st.pop();
+                op.erase(op.end()-3);
+                // if(t->right != NULL) {
+                //     st.emplace(t);     
+                //     t = t->right;
+                // } else {
+
+                // }
+            }
+        }
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
